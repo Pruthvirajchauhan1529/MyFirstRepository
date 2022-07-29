aa11cebe42ef642a68d41699c8104540082efa61
abhig47 <abhi@7span.com>
2022-07-29 11:51:31 +0530

    Making chnages on code for understanding git account

--- src/src/mypackage/abc.java
+++ src/src/mypackage/abc.java
@@ -1,10 +1,26 @@
 package mypackage;
 
+import model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class abc {
+
+    public static void studentData(List<Student> students){
+        students.add(new Student(1,"Pruthviraj"));
+        students.add(new Student(2,"Pruthviraj"));
+        students.add(new Student(4,"Pruthviraj"));
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello GIT...");
         System.out.println("Hello java....");
         System.out.println("sssxvrcsx");
         System.out.println("dvsvjsnv");
+
+        List<Student> students = new ArrayList<>();
+        studentData(students);
+        System.out.println(students);
     }
 }
