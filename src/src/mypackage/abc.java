0c2af47cdfcde666b1249021d6320b6541d37666
Pruthviraj Chauhan <pruthviraj@7span.com>
2022-07-29 11:15:45 +0530

    second changes

--- src/src/mypackage/abc.java
+++ src/src/mypackage/abc.java
@@ -3,5 +3,6 @@ package mypackage;
 public class abc {
     public static void main(String[] args) {
         System.out.println("Hello GIT...");
+        System.out.println("Hello java....");
     }
 }
